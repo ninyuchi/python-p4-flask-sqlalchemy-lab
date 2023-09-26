393335badbd9093792e6a508d51ea905ae57fed4
Nasra Kassim <ninyuchinasra@gmail.com>
2023-09-27 02:04:06 +0300

    updates

--- server/app.py
+++ server/app.py
@@ -1,34 +1,51 @@
-#!/usr/bin/env python3
-
-from flask import Flask, make_response
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
-from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-db.init_app(app)
+class Animal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    species = db.Column(db.String(100), nullable=False)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeeper.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosure.id'))
+
+class Zookeeper(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    birthday = db.Column(db.Date, nullable=False)
+    animals = db.relationship('Animal', backref='zookeeper')
+
+class Enclosure(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(100), nullable=False)
+    open_to_visitors = db.Column(db.Boolean, default=False)
+    animals = db.relationship('Animal', backref='enclosure')
 
 @app.route('/')
 def home():
-    return '<h1>Zoo app</h1>'
+    return '<h1>Welcome to the Zoo!</h1>'
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+    return render_template('animal.html', animal=animal)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    return render_template('zookeeper.html', zookeeper=zookeeper)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
-
+    enclosure = Enclosure.query.get(id)
+    return render_template('enclosure.html', enclosure=enclosure)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,23 +1,20 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
-
-class Zookeeper(db.Model):
-    __tablename__ = 'zookeepers'
-
-    id = db.Column(db.Integer, primary_key=True)
-
-class Enclosure(db.Model):
-    __tablename__ = 'enclosures'
-
-    id = db.Column(db.Integer, primary_key=True)
+from app import db
 
 class Animal(db.Model):
-    __tablename__ = 'animals'
-
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    species = db.Column(db.String(100), nullable=False)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeeper.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosure.id'))
+
+class Zookeeper(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    birthday = db.Column(db.Date, nullable=False)
+    animals = db.relationship('Animal', backref='zookeeper')
+
+class Enclosure(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(100), nullable=False)
+    open_to_visitors = db.Column(db.Boolean, default=False)
+    animals = db.relationship('Animal', backref='enclosure')
